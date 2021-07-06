0ae3713576604cc84db686bf293437f97e1e8604
Mariano-afk <blancomariano.68@gmail.com>
2021-07-06 17:29:14 -0300

    a

--- service-worker.js
+++ service-worker.js
@@ -5,6 +5,8 @@ const cacheName = 'cache-version-1';
 const precache = [
   './js/register-sw.js',
   './index.html',
+  './ranking.html',
+  './Messi.html',
   './offline.html',
   './img/Messi-offline-bar.jpg'
 ];
