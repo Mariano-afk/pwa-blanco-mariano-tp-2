d9d215b7c6f63bafc0c2d5b446c3e7a9147d7715
Mariano-afk <blancomariano.68@gmail.com>
2021-07-07 12:10:30 -0300

    a

--- service-worker.js
+++ service-worker.js
@@ -6,7 +6,7 @@ const precache = [
   './js/register-sw.js',
   './index.html',
   './offline.html',
-  './mensaje-offline.js'
+  './mensaje-offline.js',
   './logica.js',
   './img/Messi-offline-bar.jpg'
 ];
