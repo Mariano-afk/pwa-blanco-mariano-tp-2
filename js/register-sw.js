fadc7b929b1dcdd0214a5a18b9bf512f4db9f9da
Mariano-afk <blancomariano.68@gmail.com>
2021-07-07 16:35:29 -0300

    .

--- js/register-sw.js
+++ js/register-sw.js
@@ -1,6 +1,6 @@
 // Chequeo si el browser puede usar Service Worker
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('../service-worker.js')
+  navigator.serviceWorker.register('./service-worker.js')
       .then(reg => {
         console.log("Service worker esta listo!");
       });
